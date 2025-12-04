c96cf8cb57ce9fd300b2b1d88f3f3eef4835c4c8
Holger-PM <vmasson333@gmail.com>
2025-12-04 16:59:30 +0100

    Update FuchsiaGoodRodHouse.asm

--- scripts/FuchsiaGoodRodHouse.asm
+++ scripts/FuchsiaGoodRodHouse.asm
@@ -6,54 +6,54 @@ FuchsiaGoodRodHouse_TextPointers:
 	dw_const FuchsiaGoodRodHouseFishingGuruText, TEXT_FUCHSIAGOODRODHOUSE_FISHING_GURU
 	dw_const FuchsiaGoodRodHouseFishingGuideText, TEXT_FUCHSIAGOODRODHOUSE_FISHING_GUIDE
 
-FuchsiaGoodRodHouseFishingGuruText:
 	text_asm
-	ld a, [wd728]
-	bit 4, a ; got good rod?
-	jr nz, .got_item
+	CheckEvent EVENT_GOT_FUCHSIA_FOSSIL_GIFT
+	jr nz, .got_gift
 	ld hl, .Text
-	rst _PrintText
+	call PrintText
 	call YesNoChoice
 	ld a, [wCurrentMenuItem]
 	and a
 	jr nz, .refused
-	lb bc, GOOD_ROD, 1
-	call GiveItem
-	jr nc, .bag_full
-	ld hl, wd728
-	set 4, [hl] ; got good rod
-	ld hl, .ReceivedGoodRodText
-	jr .done
-.bag_full
-	ld hl, .NoRoomText
+	CheckEvent EVENT_GOT_DOME_FOSSIL
+	jr nz, .giveOmanyte
+	lb bc, KABUTO, 10
+	jr .giveGiftMon
+.giveOmanyte
+	lb bc, OMANYTE, 10
+.giveGiftMon
+	call GivePokemon
+	jp nc, TextScriptEnd
+	SetEvent EVENT_GOT_FUCHSIA_FOSSIL_GIFT
 	jr .done
 .refused
 	ld hl, .ThatsSoDisappointingText
+	call PrintText
 	jr .done
-.got_item
+.got_gift
 	ld hl, .HowAreTheFishText
+	call PrintText
 .done
-	rst _PrintText
-	rst TextScriptEnd
+	jp TextScriptEnd
 
 .Text:
 	text_far _FuchsiaGoodRodHouseFishingGuruText
 	text_end
 
-.ReceivedGoodRodText:
-	text_far _FuchsiaGoodRodHouseFishingGuruReceivedGoodRodText
+.ReceivedGiftText:
+	text_far _FuchsiaGoodRodHouseFishingGuruReceivedGiftText
 	sound_get_item_1
 	text_end
 
-;.UnusedText:
-;	para "つり　こそ"
-;	line "おとこの　ロマン　だ！"
+.UnusedText:
+	para "つり　こそ"
+	line "おとこの　ロマン　だ！"
 
-;	para "へぼいつりざおは"
-;	line "コイキングしか　つれ　なんだが"
-;	line "この　いいつりざおなら"
-;	line "もっと　いいもんが　つれるんじゃ！"
-;	done
+	para "へぼいつりざおは"
+	line "コイキングしか　つれ　なんだが"
+	line "この　いいつりざおなら"
+	line "もっと　いいもんが　つれるんじゃ！"
+	done
 
 .ThatsSoDisappointingText:
 	text_far _FuchsiaGoodRodHouseFishingGuruThatsSoDisappointingText
@@ -65,39 +65,5 @@ FuchsiaGoodRodHouseFishingGuruText:
 
 .NoRoomText:
 	text_far _FuchsiaGoodRodHouseFishingGuruNoRoomText
-	text_end
-
-FuchsiaGoodRodHouseFishingGuideText:
-	text_asm
-	CheckEvent EVENT_GOT_FISHING_GUIDE
-	ld hl, .ToAllTravelersText
-	jr nz, .print_text
-	ld hl, .ItsAFishingGuideText
-	rst _PrintText
-	SetEvent EVENT_GOT_FISHING_GUIDE
-	ld hl, .AddedToPokedexText
-	rst _PrintText
-	ld a, SFX_GET_ITEM_2 ; SFX_GET_ITEM_1 ?
-	rst _PlaySound
-	call WaitForSoundToFinish
-	ld hl, .AreaUpgradedText
-.print_text
-	rst _PrintText
-	rst TextScriptEnd
-
-.ItsAFishingGuideText
-	text_far _FuchsiaGoodRodHouseItsAFishingGuideText
-	text_end
-
-.AddedToPokedexText
-	text_far _FuchsiaGoodRodHouseAddedToPokedexText
-	text_end
-
-.AreaUpgradedText
-	text_far _FuchsiaGoodRodHouseAreaUpgradedText
-	text_end
-
-.ToAllTravelersText
-	text_far _FuchsiaGoodRodHouseToAllTravelersText
 	text_end
 
