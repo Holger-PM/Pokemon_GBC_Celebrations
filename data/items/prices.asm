3615e6b25bd58e1996d05ac10334f9a24cb9db5b
Holger-PM <vmasson333@gmail.com>
2025-12-04 06:19:01 +0100

    Items Prices Changes
    Adjusted shop item prices for game balance:
    - Increased prices of Poké Balls.
    - Reduced prices of Potions.
    - Raised prices of in-battle stat-boosting items.
    - Assigned a purchase price to PP Plus.

--- data/items/prices.asm
+++ data/items/prices.asm
@@ -1,9 +1,9 @@
 ItemPrices::
 	table_width 3, ItemPrices
 	bcd3 10000 ; MASTER_BALL
-	bcd3 1200  ; ULTRA_BALL
-	bcd3 600   ; GREAT_BALL
-	bcd3 200   ; POKE_BALL
+	bcd3 1500  ; ULTRA_BALL
+	bcd3 750   ; GREAT_BALL
+	bcd3 250   ; POKE_BALL
 	bcd3 0     ; TOWN_MAP
 	bcd3 0     ; BICYCLE
 	bcd3 0     ; SURFBOARD
@@ -17,9 +17,9 @@ ItemPrices::
 	bcd3 200   ; PARLYZ_HEAL
 	bcd3 3000  ; FULL_RESTORE
 	bcd3 2500  ; MAX_POTION
-	bcd3 1500  ; HYPER_POTION
-	bcd3 700   ; SUPER_POTION
-	bcd3 300   ; POTION
+	bcd3 1200  ; HYPER_POTION
+	bcd3 550   ; SUPER_POTION
+	bcd3 200   ; POTION
 	bcd3 0     ; BOULDERBADGE
 	bcd3 0     ; CASCADEBADGE
 	bcd3 0     ; THUNDERBADGE
@@ -40,12 +40,12 @@ ItemPrices::
 	bcd3 9800  ; CARBOS
 	bcd3 9800  ; CALCIUM
 	bcd3 4800  ; RARE_CANDY
-	bcd3 3000  ; DOME_FOSSIL
-	bcd3 3000  ; HELIX_FOSSIL
+	bcd3 0     ; DOME_FOSSIL
+	bcd3 0     ; HELIX_FOSSIL
 	bcd3 0     ; SECRET_KEY
 	bcd3 0     ; CHEAT_CANDY
 	bcd3 0     ; BIKE_VOUCHER
-	bcd3 950   ; X_ACCURACY
+	bcd3 4000  ; X_ACCURACY
 	bcd3 2100  ; LEAF_STONE
 	bcd3 0     ; CARD_KEY
 	bcd3 10000 ; NUGGET
@@ -54,20 +54,20 @@ ItemPrices::
 	bcd3 600   ; FULL_HEAL
 	bcd3 1500  ; REVIVE
 	bcd3 4000  ; MAX_REVIVE
-	bcd3 700   ; GUARD_SPEC
+	bcd3 2000  ; GUARD_SPEC
 	bcd3 500   ; SUPER_REPEL
 	bcd3 700   ; MAX_REPEL
-	bcd3 650   ; DIRE_HIT
+	bcd3 2000  ; DIRE_HIT
 	bcd3 10    ; COIN
 	bcd3 200   ; FRESH_WATER
 	bcd3 300   ; SODA_POP
 	bcd3 350   ; LEMONADE
 	bcd3 0     ; S_S_TICKET
 	bcd3 0     ; GOLD_TEETH
-	bcd3 500   ; X_ATTACK
-	bcd3 550   ; X_DEFEND
-	bcd3 350   ; X_SPEED
-	bcd3 350   ; X_SPECIAL
+	bcd3 1000  ; X_ATTACK
+	bcd3 1000  ; X_DEFEND
+	bcd3 1000  ; X_SPEED
+	bcd3 1000  ; X_SPECIAL
 	bcd3 0     ; COIN_CASE
 	bcd3 0     ; OAKS_PARCEL
 	bcd3 0     ; ITEMFINDER
@@ -78,7 +78,7 @@ ItemPrices::
 	bcd3 0     ; OLD_ROD
 	bcd3 0     ; GOOD_ROD
 	bcd3 0     ; SUPER_ROD
-	bcd3 0     ; PP_UP
+	bcd3 9800  ; PP_UP
 	bcd3 0     ; ETHER
 	bcd3 0     ; MAX_ETHER
 	bcd3 0     ; ELIXER
