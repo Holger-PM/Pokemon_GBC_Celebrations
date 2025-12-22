1db9a58deefe9ae2439b0a8c353637c272796119
Holger-PM <vmasson333@gmail.com>
2025-12-22 16:13:49 +0100

    Pokecenter & Mart tileset tweaks

--- data/maps/objects/SaffronPokecenter.asm
+++ data/maps/objects/SaffronPokecenter.asm
@@ -8,14 +8,14 @@ SaffronPokecenter_Object:
 	db $0 ; border block
 
 	def_warp_events
-	warp_event  3,  7, LAST_MAP, 7
 	warp_event  4,  7, LAST_MAP, 7
+	warp_event  5,  7, LAST_MAP, 7
 
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, STAY, DOWN, TEXT_SAFFRONPOKECENTER_NURSE
-	object_event  5,  5, SPRITE_BEAUTY, STAY, NONE, TEXT_SAFFRONPOKECENTER_BEAUTY
+	object_event  4,  1, SPRITE_NURSE, STAY, DOWN, TEXT_SAFFRONPOKECENTER_NURSE
+	object_event  6,  5, SPRITE_BEAUTY, STAY, NONE, TEXT_SAFFRONPOKECENTER_BEAUTY
 	object_event  8,  3, SPRITE_GENTLEMAN, STAY, DOWN, TEXT_SAFFRONPOKECENTER_GENTLEMAN
 	object_event 11,  2, SPRITE_LINK_RECEPTIONIST, STAY, DOWN, TEXT_SAFFRONPOKECENTER_LINK_RECEPTIONIST
 
